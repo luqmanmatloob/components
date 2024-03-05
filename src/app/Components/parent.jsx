18d058042ef3f6118bae0daf736f9f2026d94a31
luqmanmatloob <93182189+luqmanmatloob@users.noreply.github.com>
2024-03-05 17:53:42 +0500

    initial test commit

---
+++ src/app/Components/parent.jsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+
+const parent = () => {
+  return (
+    <div>
+      Parent component
+      {console.log(`Parent Component`)}
+    </div>
+  )
+}
+
+export default parent
